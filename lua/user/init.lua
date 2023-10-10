658f37f58164e3aa27f2c479bafd133799d586c2
rond <contact@rond.cc>
2023-10-10 10:20:56 +0200

    fix(init): Possible fix to lsp not being able to load keymaps

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,6 +1,6 @@
+require("user.keymaps")
 require("user.lazy")
 require("user.options")
-require("user.keymaps")
 require("user.highlight_yank")
 require("user.format_on_save")
 require("user.toggle_eslint")
