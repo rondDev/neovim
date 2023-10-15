f5095889a8b345909aa51ca10fadeb160130bf18
rond <contact@rond.cc>
2023-10-15 20:22:29 +0200

    fix(coding): combine all coding plugins into one file for better organization

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,127 @@
+return {
+	{
+		"max397574/better-escape.nvim",
+		event = { "InsertEnter" },
+		config = function()
+			require("better_escape").setup({
+				mapping = { "jk", "jj" }, -- a table with mappings to use
+				timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+				clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+				keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+				-- example(recommended)
+				-- keys = function()
+				--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+				-- end,
+			})
+		end,
+	},
+
+	{
+		"numToStr/Comment.nvim",
+		event = { "BufEnter" },
+		dependencies = {
+			-- Install context-commentstring to enable jsx commenting is ts/js/tsx/jsx files
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			--- @diagnostic disable: missing-fields
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
+
+	{
+		"dmmulroy/tsc.nvim",
+		ft = { "typescript", "typescriptreact" },
+		config = function()
+			require("tsc").setup()
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+			"windwp/nvim-autopairs",
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			--- @diagnostic disable: missing-fields
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			require("nvim-autopairs").setup()
+
+			-- Integrate nvim-autopairs with cmp
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
+					["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
+					["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+					["<C-c>"] = cmp.mapping.abort(), -- close completion window
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
+				}),
+				-- sources for autocompletion
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" }, -- lsp
+					{ name = "luasnip", max_item_count = 3 }, -- snippets
+					{ name = "buffer", max_item_count = 5 }, -- text within current buffer
+					{ name = "path", max_item_count = 3 }, -- file system paths
+				}),
+				-- Enable pictogram icons for lsp/autocompletion
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
+				experimental = {
+					ghost_text = false,
+				},
+			})
+		end,
+	},
+}
