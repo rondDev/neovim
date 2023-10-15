dd3719eb7d6396b74b9bfd5c9cb243908b5ccdbf
rond <contact@rond.cc>
2023-10-15 07:38:13 +0200

    fix(telescope-fzf): update build command
    Unsure if this is correct, but it's what's listed in the documentation

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 				cond = vim.fn.executable("make") == 1,
 			},
 		},
