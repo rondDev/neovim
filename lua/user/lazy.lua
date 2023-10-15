4c7544ab1d71e20e90935b84143492157705852d
rond <contact@rond.cc>
2023-10-15 07:39:25 +0200

    fix(noice): remove noice
    Noice removed due to just being annoying, mainly not allowing for normal
    cmdline, where it "emulates" it. And if you use that option, you have to
    mess about to get any sort of messages to show.

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -223,25 +223,6 @@ lazy.setup({
 		event = "VeryLazy",
 	},
 
-	-- TODO: re-enable noice when I figure out how it actually works
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		priority = 51,
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-		-- TODO: fix this
-	},
-
 	-- Makes using the f key trivial
 	{
 		"unblevable/quick-scope",
