d15878fde27785e321943aa88e20951652b475f5
rond <contact@rond.cc>
2023-10-15 20:15:10 +0200

    fix(configs): remove old configs in place of new structure

--- lua/plugins/tailwind_colors.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"themaxmarchuk/tailwindcss-colors.nvim",
-	event = { "BufEnter" },
-	config = function()
-		require("tailwindcss-colors").setup()
-	end,
-}
--- lua/plugins/tsc.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"dmmulroy/tsc.nvim",
-		ft = { "typescript", "typescriptreact" },
-		config = function()
-			require("tsc").setup()
-		end,
-	},
-}
