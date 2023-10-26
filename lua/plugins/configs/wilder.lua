5d0fce6d9bbccbad43e74995bd5c2ff7ef1dd1fb
rond <contact@rond.cc>
2023-10-26 11:40:04 +0200

    feat(wilder): add wilder

---
+++ lua/plugins/configs/wilder.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+M.setup = function()
+	local wilder = require("wilder")
+	wilder.setup({ modes = { ":", "/", "?" } })
+
+	-- Enable fuzzy matching for commands and buffers
+	wilder.set_option("pipeline", {
+		wilder.branch(
+			wilder.cmdline_pipeline({
+				fuzzy = 1,
+			}),
+			wilder.vim_search_pipeline({
+				fuzzy = 1,
+			})
+		),
+	})
+
+	wilder.set_option(
+		"renderer",
+		wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+			highlighter = wilder.basic_highlighter(),
+			-- highlights = {
+			-- 	default = text_highlight,
+			-- 	border = mauve_highlight,
+			-- 	accent = mauve_highlight,
+			-- },
+			pumblend = 5,
+			min_width = "100%",
+			min_height = "25%",
+			max_height = "25%",
+			border = "rounded",
+			left = { " ", wilder.popupmenu_devicons() },
+			right = { " ", wilder.popupmenu_scrollbar() },
+		}))
+	)
+end
+
+return M
