e55f1a17ca9fe15d12c956c82a45be62829a2f63
rond <contact@rond.cc>
2023-10-28 20:39:11 +0200

    fix(init): remove printing of lazy path ( was used for debugging )

--- init.lua
+++ init.lua
@@ -3,8 +3,6 @@ require("core.utils").load_mappings()
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-print(lazypath)
-
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
 	require("core.bootstrap").lazy(lazypath)
