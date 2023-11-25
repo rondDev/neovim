ad6b10231378d1f118d4e0ef96544498a828fe2f
rond <contact@rond.cc>
2023-11-25 07:43:29 +0100

    feat(plugins): add flash.nvim and remove comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,21 +45,6 @@ local plugins = {
 		end,
 	},
 
-	-- plugin for easy commenting -- TODO: test and compare to mini.comment
-	-- {
-	-- 	"numToStr/Comment.nvim",
-	-- 	event = { "InsertEnter" },
-	-- 	dependencies = {
-	-- 		-- Install context-commentstring to enable jsx commenting is ts/js/tsx/jsx files
-	-- 		"JoosepAlviste/nvim-ts-context-commentstring",
-	-- 	},
-	-- 	config = function()
-	-- 		require("Comment").setup({
-	-- 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-	-- 		})
-	-- 	end,
-	-- },
-
 	-- easy commenting
 	{
 		"echasnovski/mini.comment",
@@ -108,18 +93,7 @@ local plugins = {
 		},
 	},
 
-	-- NOTE: this might be swapped out with tpope's vim-surround, that remains to be seen
-	--
 	-- manage parentheses, brackets, quotes etc.
-	-- {
-	-- 	"echasnovski/mini.surround",
-	-- 	version = false,
-	-- 	config = function()
-	-- 		require("mini.surround").setup()
-	-- 	end,
-	-- },
-
-	--
 	{
 		"tpope/vim-surround",
 	},
@@ -172,6 +146,39 @@ local plugins = {
 			"ibhagwan/fzf-lua",
 		},
 		config = true,
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			modes = {
+				char = {
+					jump_labels = true,
+				},
+			},
+		},
+		keys = {
+			-- {
+			-- 	"f",
+			-- 	mode = { "n", "x", "o" },
+			-- 	function()
+			-- 		require("flash").jump()
+			-- 	end,
+			-- 	desc = "Flash",
+			-- },
+			-- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			-- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		},
 	},
 
 	--#endregion
