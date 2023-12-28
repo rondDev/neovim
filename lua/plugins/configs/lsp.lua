eb52d3efd825f416d5559607b6e0bd4cec8fbae9
rond <contact@rond.cc>
2023-12-28 16:13:57 +0100

    fix(lsp mappings): change to <leader>p instead of <leader>ps

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -43,7 +43,7 @@ M.map_lsp_keybinds = function(buffer_number)
 	)
 
 	M.nnoremap(
-		"<leader>ps",
+		"<leader>p",
 		require("telescope.builtin").lsp_workspace_symbols,
 		{ desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
 	)
