5d8bcbf9705d85a22a879907c71b586a1736a967
rondDev <contact@rond.cc>
2024-07-23 12:49:11 +0200

    fix: add dependency for nvim-dap-ui

--- lua/plugins/configs/dap.lua
+++ lua/plugins/configs/dap.lua
@@ -2,7 +2,7 @@ return {
 	{ "mfussenegger/nvim-dap" },
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap" },
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		config = function()
 			require("dapui").setup()
 		end,
