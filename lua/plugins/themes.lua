3e1c4bd1ab75b76ec5ba82451941d3a39f467cab
rondDev <contact@rond.cc>
2025-02-21 14:28:19 +0100

    feat: add eldritch theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,4 +2,13 @@ return {
 	{
 		"nyoom-engineering/oxocarbon.nvim",
 	},
+	{
+		"eldritch-theme/eldritch.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd.colorscheme("eldritch")
+		end,
+	},
 }
