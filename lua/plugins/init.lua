97443d51e0b3dfc565a80c4a07bebef3e6e12d56
rondDev <contact@rond.cc>
2025-02-22 22:58:24 +0100

    feat: add neogit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -284,4 +284,24 @@ return {
 		--   skip_filetypes = { 'lua', 'typescript' },
 		-- }
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			-- "nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			-- "echasnovski/mini.pick", -- optional
+		},
+		-- config = true,
+		keys = {
+			{
+				"<leader>gg",
+				"<cmd>Neogit<CR>",
+				desc = "NeoGit",
+			},
+		},
+	},
 }
