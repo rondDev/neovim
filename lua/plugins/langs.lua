d1e5c7262634812d125f753a88b309d8f7fceab8
rondDev <contact@rond.cc>
2025-02-23 15:38:20 +0100

    fix: formatting
    feat: add event to typescript

--- lua/plugins/langs.lua
+++ lua/plugins/langs.lua
@@ -1,50 +1,51 @@
 return {
-	-- Zig.
-	{
-		"ziglang/zig.vim",
-		ft = "zig",
-	},
+  -- Zig.
+  {
+    "ziglang/zig.vim",
+    ft = "zig",
+  },
 
-	-- Rust
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false, -- This plugin is already lazy
-	},
-	-- {
-	-- 	"simrat39/rust-tools.nvim",
-	-- 	ft = "rust",
-	-- },
+  -- Rust
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  -- {
+  -- 	"simrat39/rust-tools.nvim",
+  -- 	ft = "rust",
+  -- },
 
-	-- typescript
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
-	},
+  -- typescript
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    lazy = true,
+    opts = {},
+  },
 
-	-- haskell
-	{
-		"mrcjkb/haskell-tools.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		version = "^3", -- Recommended
-		ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-	},
+  -- haskell
+  {
+    "mrcjkb/haskell-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    version = "^3", -- Recommended
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  },
 
-	-- go
-	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("go").setup()
-		end,
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	},
+  -- go
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
