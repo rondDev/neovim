54ef477be9e3bc9a487a5c49e0ec6651e135b757
rondDev <contact@rond.cc>
2025-02-23 14:11:58 +0100

    feat: add lazy nvim to legendary (needs debugging)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -247,6 +247,9 @@ return {
 		lazy = false,
 		-- sqlite is only needed if you want to use frecency sorting
 		dependencies = { "kkharji/sqlite.lua" },
+		config = function()
+			require("legendary").setup({ extensions = { lazy_nvim = true } })
+		end,
 		keys = {
 			{
 				"<M-x>",
