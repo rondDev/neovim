7b65d4d05cbfcf98fbc295f2eddc6b8e20d4cac2
rondDev <contact@rond.cc>
2025-04-05 14:44:17 +0200

    fix: disable stylua (caused formatting conflict)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -206,7 +206,7 @@ return {
 
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.formatting.stylua,
+					-- null_ls.builtins.formatting.stylua,
 					-- null_ls.builtins.completion.spell,
 				},
 			})
